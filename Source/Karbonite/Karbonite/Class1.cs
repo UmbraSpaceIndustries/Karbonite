e9b65e209cfde66cc5dc2bd4b0c04de166195116
Bob Palmer <rbpalmer2222@gmail.com>
2014-07-24 08:58:09 -0700

    Plugin code, test configs.

---
+++ Source/Karbonite/Karbonite/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Karbonite
+{
+    public class KarboniteConverter : PartModule
+    {
+        [KSPField] 
+        public string convertAnimationName = "Convert";
+
+        private bool _isConverting;
+        private List<ModuleGenerator> _generators;
+
+        public Animation ConvertAnimation
+        {
+            get
+            {
+                return part.FindModelAnimators(convertAnimationName)[0];
+            }
+        }
+
+        public override void OnStart(PartModule.StartState state)
+        {
+            FindGenerators();
+            ConvertAnimation[convertAnimationName].layer = 3;
+        }
+
+        public override void OnLoad(ConfigNode node)
+        {
+            FindGenerators();
+        }
+
+        public override void OnAwake()
+        {
+            FindGenerators();
+        }
+
+        public override void OnUpdate()
+        {
+            CheckForConverting();
+            base.OnUpdate();
+        }
+
+        private void FindGenerators()
+        {
+            if (vessel != null)
+            {
+                if (part.Modules.Contains("ModuleGenerator"))
+                {
+                    _generators = part.Modules.OfType<ModuleGenerator>().ToList();
+                } 
+            }
+        }
+
+        private void CheckForConverting()
+        {
+            if(_generators.Any(g=>g.generatorIsActive))
+            {
+                if (!ConvertAnimation.isPlaying)
+                {
+                    ConvertAnimation[convertAnimationName].speed = 1;
+                    ConvertAnimation.Play(convertAnimationName);
+                }
+            }
+        }
+    }
+}
