48108cf88cc521bfc1d6ad2df8891767b9798343
Bob Palmer <rbpalmer2222@gmail.com>
2014-12-10 22:33:17 -0500

    Switched to Regolith

---
+++ Source/Karbonite/Karbonite/Karbonite.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+namespace Karbonite
+{
+    public class Karbonite
+    {
+        //Just here for MM
+    }
+}
+
